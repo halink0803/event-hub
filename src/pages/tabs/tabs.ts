56e836115d6cb962925afd6fb4fbff7f11d3d113
halink0803 <halink0803@gmail.com>
2017-02-07 11:54:07 +0700

    add login page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
+import { LoginPage } from '../login/login';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,7 +11,7 @@ import { ContactPage } from '../contact/contact';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = HomePage;
+  tab1Root: any = LoginPage;
   tab2Root: any = AboutPage;
   tab3Root: any = ContactPage;
 
