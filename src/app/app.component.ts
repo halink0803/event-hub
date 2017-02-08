d1bb10bd0e3508fe5de56f0103056e1b2fe2b2b5
halink0803 <halink0803@gmail.com>
2017-02-08 15:37:10 +0700

    update notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform, MenuController, Nav } from 'ionic-angular';
+import { Platform, MenuController, Nav,  AlertController } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { FeedsPage } from '../pages/feeds/feeds';
 import { LoginPage } from '../pages/login/login';
+import { Push } from 'ionic-native';
 
 
 @Component({
@@ -16,7 +17,7 @@ export class MyApp {
   rootPage = FeedsPage;
   menus : Array<{title: string, component: any}>;
 
-  constructor(platform: Platform, public menu: MenuController, ) {
+  constructor(public platform: Platform, public menu: MenuController, public alertCtrl: AlertController) {
 
     this.menus = [
       {title: 'Login', component: LoginPage},
@@ -33,7 +34,53 @@ export class MyApp {
   }
 
   initPushNotification() {
-    
+    if(!this.platform.is('cordova')) {
+      console.warn("Push noitification not initialized. Cordova is not available - Run in physical device");
+      return;
+    }
+    let push = Push.init({
+      android: {
+        senderID: "972428714274"
+      },
+      ios: {
+        alert: "true",
+        badge: false,
+        sound: "true"
+      },
+      windows: {}
+    });
+
+    push.on('registration', (data) => {
+      console.log("device token ->", data.registrationId);
+      //TODO - send device token to server
+    });
+    push.on('notification', (data) => {
+      console.log('message', data.message);
+      let self = this;
+
+      if(data.additionalData.foreground) {
+        let confirmAlert = this.alertCtrl.create({
+          title: 'New Notification',
+          message: data.message,
+          buttons: [{
+            text: 'Ignore',
+            role: 'cancel'
+          }, {
+            text: 'View',
+            handler: () => {
+              self.nav.push(FeedsPage, {message: data.message})
+            }
+          }]
+        });
+        confirmAlert.present();
+      } else {
+        self.nav.push(FeedsPage, {message: data.message});
+        console.log("Push notification clicked");
+      }
+    });
+    push.on('error', (e) => {
+      console.log(e.message);
+    });
   }
 
   openPage(page) {
